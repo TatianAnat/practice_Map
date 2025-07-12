b89f87f7958fab6457b100b67e6064aaddd58e12
TatianAnat <TatianAnat@yandex.ru>
2025-07-13 00:07:07 +0400

    practice HashMap

--- src/JavaProfMain.java
+++ src/JavaProfMain.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class JavaProfMain {
     public static void main(String[] args) {
@@ -19,7 +21,7 @@ public class JavaProfMain {
         strings.add("11");
         System.out.println("strings = " + strings);
 
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         map.put("1", "one");
         map.put("2", "two");
         map.put("3", "two");
@@ -32,5 +34,68 @@ public class JavaProfMain {
         map.put("10", "one");
         map.put("11", "one");
         System.out.println("map = " + map);
+
+        Map<Object, Object> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("1", "one");
+        linkedHashMap.put("2", "two");
+        linkedHashMap.put("3", "two");
+        linkedHashMap.put("4", "two");
+        linkedHashMap.put("5", "one");
+        linkedHashMap.put("6", "one");
+        linkedHashMap.put("7", "one");
+        linkedHashMap.put("8", "one");
+        linkedHashMap.put("9", "one");
+        linkedHashMap.put("10", "one");
+        linkedHashMap.put("11", "one");
+        System.out.println("map = " + linkedHashMap);
+        System.out.println("map = " + linkedHashMap.get("1"));
+
+        HashMap<MyKey, MyValue> colors = new HashMap<>();
+        colors.put(new MyKey("black"),new MyValue(0,0,0));
+        colors.put(new MyKey("white"),new MyValue(255,255,255));
+        colors.put(new MyKey("red"),new MyValue(255,0,0));
+        System.out.println("colors = " + colors);
+        System.out.println("colors " + colors.get("white"));
+
+    }
+/**
+ * создаём класс, который будет хранить строчку
+ */
+static class MyKey {
+    String color;
+
+    public MyKey(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "MyKey{" +
+                "color='" + color + '\'' +
+                '}';
+    }
+}
+    /**
+     * соответствие каждому цвету некоторого набора  RGB
+     */
+    static class MyValue {
+        int red;
+        int green;
+        int blue;
+
+        public MyValue(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        @Override
+        public String toString() {
+            return "MyValue{" +
+                    "red=" + red +
+                    ", green=" + green +
+                    ", blue=" + blue +
+                    '}';
+        }
     }
 }
